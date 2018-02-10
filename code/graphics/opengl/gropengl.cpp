dd42f700ee2b5edc3346582d3ad917f2f1b8ff96
asarium <asarium@gmail.com>
2018-02-10 15:17:34 +0100

    Add persistent mapping support

--- code/graphics/opengl/gropengl.cpp
+++ code/graphics/opengl/gropengl.cpp
@@ -1140,6 +1140,8 @@ void opengl_setup_function_pointers()
 	gr_screen.gf_delete_buffer		= gr_opengl_delete_buffer;
 	gr_screen.gf_update_buffer_data		= gr_opengl_update_buffer_data;
 	gr_screen.gf_update_buffer_data_offset	= gr_opengl_update_buffer_data_offset;
+	gr_screen.gf_map_buffer = gr_opengl_map_buffer;
+	gr_screen.gf_flush_mapped_buffer = gr_opengl_flush_mapped_buffer;
 	gr_screen.gf_bind_uniform_buffer = gr_opengl_bind_uniform_buffer;
 
 	gr_screen.gf_update_transform_buffer	= gr_opengl_update_transform_buffer;
@@ -1593,6 +1595,8 @@ bool gr_opengl_is_capable(gr_capability capability)
 		return !Cmdline_no_geo_sdr_effects;
 	case CAPABILITY_TIMESTAMP_QUERY:
 		return GLAD_GL_ARB_timer_query != 0; // Timestamp queries are available from 3.3 onwards
+	case CAPABILITY_PERSISTENT_BUFFER_MAPPING:
+		return GLAD_GL_ARB_buffer_storage != 0;
 	}
 
 	return false;
